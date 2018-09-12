e17957f56f5db9c264acd639618a1cb85893264f
fuziwang <2622860598@qq.com>
2018-09-12 21:00:07 +0800

    process/information calc

---
+++ nodejs-demo/04-process/01-information.js
@@ -0,0 +1,36 @@
+#!/usr/bin/node
+
+const log = console.log;
+
+/*向控制台打印 CPU 架构信息以及操作系统版本信息*/
+log("arch: ",process.arch);
+log("platform: ",process.platform);
+
+/*打印进程 id 信息以及 Node.js 可执行文件的绝对路径信息*/
+log("id: ",process.pid);
+log("execPath: ",process.execPath);
+
+/*在脚本中让程序暂停执行*/
+process.stdin.pause();
+
+/*打印 Node.js 版本信息，当前登录用户 id 信息，当前登录用户所属组 id 信息以及当前脚本所在路径信息*/
+
+log("version: ",process.version);
+log("uid: ",process.getuid());
+log("gid: ",process.getgid());
+log("pwd: ",process.cwd());
+
+/*打印内存使用情况*/
+log("rss: ",process.memoryUsage().rss);
+log("heapTotal: ",process.memoryUsage().heapTotal);//栈的总量
+log("heapUsed: ",process.memoryUsage().heapUsed);//栈的使用量
+log("external: ",process.memoryUsage().external);//Chrome v8管理的c++内存
+
+/*打印环境变量*/
+log("env: ",process.env);
+
+
+
+
+
+
---
+++ nodejs-demo/04-process/02-calc.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+/*
+console.log(process.argv.length);
+
+for(var i = 0;i<process.argv.length;i++){
+  console.log(`${i}:argv - ${process.argv[i]}`);
+}
+*/
+
+if(process.argv.length === 2 || process.argv[2] == "--help" || process.argv[2] == "-h"){
+  console.log("usage: cmd-name [OPTION] [expression]\nevaluate the expression.\n\nMandatory arguments to long options are mandatory for short options too.\n-h, --help output help information and exit"
+      )} else{
+  console.log(process.argv[2] + "=" + eval(process.argv[2]));        
+}
