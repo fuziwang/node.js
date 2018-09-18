5624f88be555c8f6b2edf78515e3fa80fd5c06de
fuziwang <2622860598@qq.com>
2018-09-18 17:57:50 +0800

    settimeout/cleartimeout/setinterval/clearinterval

---
+++ nodejs-demo/05-timer/01-delay-call.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+
+/*console.log('first');
+
+global.setTimeout(function(){
+  console.log('second');
+},2000);
+//说明JavaScript的异步执行
+console.log('third');*/
+
+function Bomb(){
+  this.message = 'Bomb';
+}
+
+Bomb.prototype.explode = function(){
+  console.log(this.message);
+}
+
+var b = new Bomb();
+
+// 此时直接执行时，this指向的是timeout对象
+var time = global.setTimeout(b.explode.bind(b),2000);
+
+//global.clearTimeout(time);
+
---
+++ nodejs-demo/05-timer/02-timer.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+
+console.log("start");
+
+const time = global.setInterval(loop,500);
+
+function loop(){
+  console.log('I will loop forever');
+}
+
+global.setTimeout(function(){
+  global.clearInterval(time);
+  console.log('end');
+},3000);
