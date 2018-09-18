598499c0d557d6ed99469efec0f15f762877a818
fuziwang <2622860598@qq.com>
2018-09-18 17:04:40 +0800

    process:exit/stdio/signal

---
+++ nodejs-demo/04-process/03-exit-code.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+var code = process.argv[2];
+if(typeof code === 'number'){
+  console.log("命令行参数类型不符合！");
+  process.exit(1);
+}
+if(process.argv.length <3){
+  console.error("请给出命令行参数！");
+  process.exit(1);
+}
+process.exit(code);
+
+// process.exit(code:number) ?表示可以有也可以表示没有
+// 获取退出码 echo $?
